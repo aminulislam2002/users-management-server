d52eea250d50a792911c47854aa984c96b521ace
aminulislam2002 <aminulislamemon2002@gmail.com>
2023-05-07 07:33:38 -0700

    finish users management server

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
+
 app.use(cors());
+app.use(express.json());
 
 const users = [
   { id: 1, name: "Sabana", email: "sabana@gmail.com" },
@@ -16,6 +18,15 @@ app.get("/", (req, res) => {
 
 app.get("/users", (req, res) => {
   res.send(users);
+});
+
+app.post("/users", (req, res) => {
+  console.log("post api hitting");
+  console.log(req.body);
+  const newUser = req.body;
+  newUser.id = users.length + 1;
+  users.push(newUser);
+  res.send(newUser)
 });
 
 app.listen(port, () => {
